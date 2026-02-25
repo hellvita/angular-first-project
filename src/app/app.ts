d531e733d4a19a6c6d2a1cdb8458b0cf0e20aea4
hellvita <seredaolga03@gmail.com>
2026-02-25 14:35:19 +0200

    add the test-component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TestComponent } from './components/test-component/test-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TestComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
